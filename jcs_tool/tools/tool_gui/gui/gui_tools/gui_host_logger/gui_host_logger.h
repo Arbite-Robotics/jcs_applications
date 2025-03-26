f8f45a02faf408b623e531fc5b768db33f2552c0
Laurence McLean <lmclean81@gmail.com>
2025-03-26 10:33:52 +1030

    gui_tool: Move host logger to use sampler

--- jcs_tool/tools/tool_gui/gui/gui_tools/gui_host_logger/gui_host_logger.h
+++ jcs_tool/tools/tool_gui/gui/gui_tools/gui_host_logger/gui_host_logger.h
@@ -10,10 +10,9 @@
 #include "gui_device_host_base.h"
 #include <vector>
 #include <string>
-#include <array>
-#include "gui_stimulus.h"
 #include "imgui.h"
-#include "helpers.h"
+
+#include "sampler.h"
 
 class gui_host_logger : public gui_type_base, public gui_device_host_base {
 public:
@@ -22,60 +21,12 @@ public:
 
     int startup();
     int step_rt();
-    // int step_rt_special();
     int step_rt_always();
     int render();
 
 private:
-    // Sampler
-    enum class sampler_state {
-        off_s,
-        sampling_s,
-    };
-    sampler_state sampler_state_;
-
-    int sample_rate_;
-    int sample_time_;
-    int display_points_;
-
-    int storage_count_;
-    int storage_length_;
-
-    struct channel {
-        std::string name_;
-        std::string source_;
-        int source_combo_idx_;
-        bool fit_y_;
-        bool fit_x_;
-
-        helpers::scrolling_buffer buffer_;
-        int sample_time_s_;
-        int sample_rate_;
-        int downsample_points_;
-
-        bool plot_cursors_;
-        double cursor_tag_[4];
-
-        channel(std::string const& name, std::string const& source, int const sample_time_s, int const sample_rate_hz);
-
-        void update_sample_rate(int sample_rate_hz);
-        void update_storage_length(int sample_time_s);
-        void update_display_downsample_points(int downsample_points);
-        void plot();
-        void clear();
-    };
-    std::array<channel*, 8> channels_;
-
-    std::vector<float> f32_osignal_store_;
-
-    double t_start_ns_;
-    float t_s_;
-
-    int render_plot();
-    int render_interface();
-    void render_status();
-    int write_channels_to_file();
-    int emit_data(std::string const& path_and_file);
+    sampler sampler_;
+    std::vector<float> f32_output_signal_store_;
 };
 
 #endif
