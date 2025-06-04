89e526b7f7ee931aefdea8b113862f25991f2b38
Laurence McLean <lmclean81@gmail.com>
2025-06-04 11:12:57 +0930

    jcs_tool: Update dev_braking_chopper parameters for oscilloscope support

--- jcs_tool/tools/tool_gui/gui/gui_device/gui_device_braking_chopper.cpp
+++ jcs_tool/tools/tool_gui/gui/gui_device/gui_device_braking_chopper.cpp
@@ -3,6 +3,7 @@
 //
 #include "gui_device_braking_chopper.h"
 #include "gui_parameter.h"
+#include "gui_oscilloscope.h"
 #include "gui_firmware_update.h"
 #include <iostream>
 
@@ -11,8 +12,15 @@
 gui_device_braking_chopper::gui_device_braking_chopper(jcs::jcs_host* host, gui_interface* gui_if, std::string const& name) :
     gui_device_base(host, gui_if, name)
 {
-    gui_element_.push_back(new gui_parameter(host_, gui_if_, name_, 
-        &jcs::node_parameter::dev_braking_chopper::parameters, &jcs::node_parameter::dev_braking_chopper::parameter_enums));
+    gui_element_.push_back(new gui_parameter(host_, gui_if_, name_,
+        &jcs::node_parameter::dev_braking_chopper::parameters,
+        &jcs::node_parameter::dev_braking_chopper::parameter_enums));
+    gui_element_.push_back(new gui_oscilloscope(host_, gui_if_, name_,
+        &jcs::node_parameter::dev_braking_chopper::oscilloscope_sources,
+        &jcs::node_parameter::dev_braking_chopper::oscilloscope_trigger_config,
+        jcs::node_parameter::dev_braking_chopper::oscilloscope_sample_rate_hz,
+        jcs::node_parameter::dev_braking_chopper::oscilloscope_sample_length,
+        jcs::node_parameter::dev_braking_chopper::oscilloscope_n_channels));
     gui_element_.push_back(new gui_firmware_update(host_, gui_if_, name_));
 }
 
